e55d1e539df3fea60e84d2025ea9ba9d6d033ad8
Mehrdad Safari <m.safari@zadak.ir>
2023-12-20 14:59:54 +0330

    Bug Fix Gradle

--- ArTalkNotificationCore/build.gradle.kts
+++ ArTalkNotificationCore/build.gradle.kts
@@ -34,11 +34,11 @@ android {
 }
 
 dependencies {
-
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation(files("libs/serviceLibrary-release.aar"))
+    implementation(files("./libs/serviceLibrary-release.aar"))
 
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-messaging:23.4.0")
