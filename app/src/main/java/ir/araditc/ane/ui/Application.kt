33e023cbed2a694a44e42781d41be5a581579b1f
Mehrdad Safari <m.safari@zadak.ir>
2023-12-25 11:41:00 +0330

    Bug Fix SDK

--- app/src/main/java/ir/araditc/ane/ui/Application.kt
+++ app/src/main/java/ir/araditc/ane/ui/Application.kt
@@ -7,6 +7,8 @@ import ir.araditc.anc.model.FirebaseConfig
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
-        Arad.init(this , FirebaseConfig("AIzaSyA4OoILi_0g9xyX5wvYE9EbUOEBbpwUmvs", "1:522440954210:android:a29e3b6664dfec4bf2dc3b" , "omidbank-3dd96"))
+
+        // Init Arad Push Android SDK
+        Arad.init(this , FirebaseConfig("{FCM_API_KEY}", "{FCM_APPLICATION_ID}" , "{FCM_PROJECT_ID}"))
     }
 }
